c2bd2897c3b64a2d7bf61aba9386068fa2d1ce73
AlexLi88 <alexlxt88@gmail.com>
2014-11-25 20:06:09 -0800

    Add a new sound track

--- synth-brewery API/SynthBrewery.java
+++ synth-brewery API/SynthBrewery.java
@@ -4,47 +4,122 @@ import java.io.FileInputStream;
 import java.io.FileInputStream;
 import javax.sound.sampled.*; 
 
-public class SynthBrewery 
+public class SynthBrewery
 {
-	public static void main(String[] args)
-	{
+	public static void main(String[] args){
 
 		//Pre-computations
 		final double twopi = 2*Math.PI; 
 
 		//Set Options
-		final int AMPLITUDE_MAX = 18000;
-		final int AMPLITUDE_START = 4000;
+		final int AMPLITUDE_MAX = 10000;
+		final int AMPLITUDE_START = 8000;
 		final int SAMPLING_RATE = 44100; 
 
 		/* 
-		This creates an Audio Track with 8 beats 120bpm (beats/min)
+		This creates an Audio Track with a length in beats and a specified bpm (beats/min)
 		Note: Do not confuse this with an Android AudioTrack, 
-		this is the AuioTrack API inside AuioTrack.java
+		this is the AuioTrack class from Synth-Brewery API 
 		*/
-		AudioTrack track1 = new AudioTrack(SAMPLING_RATE, 8, 120);
+		int song_length = 24; 
+		double song_bpm= 100;
+
+		//AudioTrack track1 = new AudioTrack(SAMPLING_RATE, song_length, song_bpm, WaveType.VIOLIN);
+		//AudioTrack track2 = new AudioTrack(SAMPLING_RATE, song_length, song_bpm, WaveType.CLARINET);
+		AudioTrack track3 = new AudioTrack(SAMPLING_RATE, song_length, song_bpm, WaveType.FLUTE);
+
+		//Initialize a tuning object so we can get Equally tempered frequencies
+		Tuning et = new Tuning(TuningMode.EQUALLY_TEMPERED); 
+		for(int i = 0; i < 2; i++){
+			track3.writeBeat(et.getNoteFreq("E_4"), AMPLITUDE_START);
+		}
 		
-
-		//Let's test our track1 by writing 8 beats
-		for (int i = 0; i < 2; i++){ //write 2 beats of 440 (A4)
-			track1.writeBeat(440, AMPLITUDE_START); 
+		
+		
+		for(int i = 0; i < 1; i++){
+			track3.writeBeat(et.getNoteFreq("F_4"), AMPLITUDE_START);
 		}
-
-		for (int i = 0; i < 2; i++){ //Write 2 beats of A4 M3
-			track1.writeBeat(440*5/3, AMPLITUDE_START);
+		
+		for(int i = 0; i < 2; i++){
+			track3.writeBeat(et.getNoteFreq("G_4"), AMPLITUDE_START);
 		}
-
-		for (int i = 0; i < 2; i++){ //Write 2 beats of A4 P5
-			track1.writeBeat(440*3/2, AMPLITUDE_START);
+		
+		
+		for(int i = 0; i < 1; i++){
+			track3.writeBeat(et.getNoteFreq("F_4"), AMPLITUDE_START);
 		}
-
-		for (int i = 0; i < 2; i++){ //Write 2 beats of A4
-			track1.writeBeat(440, AMPLITUDE_START); 
+		
+		for(int i = 0; i < 1; i++){
+			track3.writeBeat(et.getNoteFreq("E_4"), AMPLITUDE_START);
 		}
+		
+		for(int i = 0; i < 1; i++){
+			track3.writeBeat(et.getNoteFreq("D_4"), AMPLITUDE_START);
+		}
+		
+		for(int i = 0; i < 2; i++){
+			track3.writeBeat(et.getNoteFreq("C_4"), AMPLITUDE_START);
+		}
+		
+		for(int i = 0; i < 1; i++){
+			track3.writeBeat(et.getNoteFreq("D_4"), AMPLITUDE_START);
+		}
+		
+		for(int i = 0; i < 2; i++){
+			track3.writeBeat(et.getNoteFreq("E_4"), AMPLITUDE_START);
+		}
+		
+		for(int i = 0; i < 2; i++){
+			track3.writeBeat(et.getNoteFreq("D_4"), AMPLITUDE_START);
+		}
+		//Let's test our API by writing a song!
+//		for (int i = 0; i < 1; i++){ //write 2 beats of 440 (A4)
+//			//track1.writeBeat(et.getNoteFreq("C_4"), AMPLITUDE_START); 
+//			track2.writeBeat(et.getNoteFreq("E_4"), AMPLITUDE_START);
+//		}
+
+//		for (int i = 0; i < 2; i++){ //Write 2 beats of A4 M3
+//			track1.writeBeat(et.getNoteFreq("G_4"), AMPLITUDE_START);
+//			track2.writeBeat(et.getNoteFreq("C_3"), AMPLITUDE_START);
+//		}
+//
+//		for (int i = 0; i < 2; i++){ //Write 2 beats of A4 P5
+//			track1.writeBeat(et.getNoteFreq("A_4"), AMPLITUDE_START);
+//			track2.writeBeat(et.getNoteFreq("F_3"), AMPLITUDE_START);
+//		}
+//
+//		for (int i = 0; i < 2; i++){ //Write 2 beats of A4 P5
+//			track1.writeBeat(et.getNoteFreq("G_4"), AMPLITUDE_START);
+//			track2.writeBeat(et.getNoteFreq("C_3"), AMPLITUDE_START);
+//		}
+//
+//		for (int i = 0; i < 2; i++){ //Write 2 beats of A4 P5
+//			track1.writeBeat(et.getNoteFreq("F_4"), AMPLITUDE_START);
+//			track2.writeBeat(et.getNoteFreq("F_3"), AMPLITUDE_START);
+//		}
+//
+//		for (int i = 0; i < 2; i++){ //Write 2 beats of A4 P5
+//			track1.writeBeat(et.getNoteFreq("E_4"), AMPLITUDE_START);
+//			track2.writeBeat(et.getNoteFreq("C_3"), AMPLITUDE_START);
+//		}
+//
+//		for (int i = 0; i < 2; i++){ //Write 2 beats of A4 P5
+//			track1.writeBeat(et.getNoteFreq("D_4"), AMPLITUDE_START);
+//			track2.writeBeat(et.getNoteFreq("G_3"), AMPLITUDE_START);
+//		}
+//
+//		for (int i = 0; i < 2; i++){ //Write 2 beats of A4 P5
+//			track1.writeBeat(et.getNoteFreq("C_4"), AMPLITUDE_START);
+//			track2.writeBeat(et.getNoteFreq("C_3"), AMPLITUDE_START);
+//		}
+
+		Mixer mixer = new Mixer(song_length, song_bpm, SAMPLING_RATE);
+//		mixer.addTrack(track1, 0); 
+		mixer.addTrack(track3, 0); 
 
 
+		//Using standard java libraries for sound playback
 		try{
-
 			//instantiate javax.sound.sampled structures for playback
 			AudioFormat audioFormat = new AudioFormat(SAMPLING_RATE, 16, 1, true, false);
 			SourceDataLine dataLine = AudioSystem.getSourceDataLine(audioFormat);
@@ -52,11 +127,22 @@ public class SynthBrewery
 			dataLine.start(); 
 
 			//Obtain the samples array of track1 in bytes
-			byte[] track1_bytes = track1.getByteArray(); 
+			byte[] mixer_bytes = mixer.getByteArray(); 
 			
 			//This writes the audio to the audio buffer and plays it
 			System.out.println("Playing: Writing to audio Buffer..");
-			dataLine.write(track1_bytes, 0, track1_bytes.length);
+
+			int i = 0; 
+			while(i < mixer_bytes.length){
+				byte[] temp = new byte[SAMPLING_RATE];
+				for(int j = 0; j < SAMPLING_RATE; j++){		
+					if (i >= mixer_bytes.length){
+						break;
+					}
+					temp[j] = mixer_bytes[i++];
+				}
+				dataLine.write(temp, 0, temp.length);
+			}
 
 		} catch (Exception e){
 			e.printStackTrace(); 
