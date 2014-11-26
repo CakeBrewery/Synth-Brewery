78eb8f079942613d474ce9b82bb758b8460275af
AlexLi88 <alexlxt88@gmail.com>
2014-11-25 20:07:14 -0800

    New wave form added

--- synth-brewery API/WaveForm.java
+++ synth-brewery API/WaveForm.java
@@ -4,8 +4,8 @@
  * specific phase indexes (in radians)
  */
 
-public class WaveForm {	
-	
+public class WaveForm
+{	
 	//Wave configuration attributes
 	int sampling_rate; 
 	int amplitude;
@@ -55,11 +55,65 @@ public class WaveForm {
 		
     	return sample; 
     }
-    
-    //In the future this will allow the user to create his own formulas for waves
-    private double sampleCustom(){
-    	return (double)(this.amplitude*Math.sin(this.phase_index));
+
+    //This math formula tries to digitally emulate a violin
+    /* how it works: 
+    	see http://js.do/blog/sound-waves-with-javascript/
+    	The sound is a sum of harmonics with decreasing
+    	amplitudes
+    */
+    private double sampleViolin(){
+    	int i = (int)((phase_index*sampling_rate)/(double)(current_frequency*twopi));
+    	double t = (double)i/(double)sampling_rate;
+
+    	double sample; 
+    	double  y = 0; 
+    	double A_total = 0; 
+
+    	for(int harm = 1; harm < 7; harm++){
+    		double f2 = (double)current_frequency*(double)harm;
+    		double A = (double)1/harm; 
+   			A_total += (double)A;
+   			y += A*Math.sin(f2*twopi*t);
+    	}
+
+    	sample = y/A_total;
+    	sample *= (1-0.5*Math.sin(twopi*6*t));
+    	sample *= (1-Math.exp(-(t*3)));
+    	sample *= amplitude*2;
+    	
+    	return sample; 
     }
+    
+    //This method tried to simulate a bell. 
+    //Not works well. 
+    private double sampleBell(){
+    	double samples;
+    	int i = (int)((phase_index*sampling_rate)/(double)(current_frequency*twopi));
+    	double t = (double)i/(double)sampling_rate;
+    	double w = 2 * pi * (double)current_frequency*t;
+    	samples = Math.cos(w + 8*Math.sin(w * 7 /5) * Math.exp(-t*4));
+    	samples *= Math.exp(-t*3);
+    	samples *= amplitude*2;
+    	return samples;
+    }
+    
+    
+    //This method tries to simulate a Flute sound.
+    private double sampleFlute(){
+    	double samples;
+    	int i = (int)((phase_index*sampling_rate)/(double)(current_frequency*twopi));
+    	double t = (double)i/(double)sampling_rate;
+    	double w = 2 * pi * (double)current_frequency * t;
+    	samples = (Math.sin(w) + 0.75 * Math.sin(w*3) + 0.5 * Math.sin(w * 5) + 0.14 * Math.sin(w *7) + 0.5 * Math.sin(w * 9) + 0.12 * Math.sin(w * 11) + 0.17 * Math.sin(w * 13)) / (1 + 0.75 + 0.5 + 0.14 + 0.17);
+    	samples *= Math.exp(t/1.5); 
+    	samples *= Math.exp(-1 * t * 1.5);
+    	return samples;
+    }
+    //In the future this will allow the user to create his own formulas for waves
+    //They just have to override the following two methods with their own math
+    private double sampleCustom(){return -1;}
+    public void incPhaseIndexCustom(){}
 	
 	/* Returns a sample of the waveform at the current phase index */
 	public double getSample(){
@@ -77,6 +131,14 @@ public class WaveForm {
 		case CUSTOM:
 			sample = sampleCustom();
 			break; 
+		case VIOLIN:
+			sample = sampleViolin();
+			break; 
+		case BELL:
+			sample = sampleBell();
+			break;
+		case FLUTE:
+			sample = sampleFlute();
 		default:
 			sample = sampleSine();
 			break;
@@ -84,11 +146,21 @@ public class WaveForm {
 		
 		return sample; 			
 	}	
-	
+
 	//This function increases the wave's sample offset (or phase index)
 	public void incPhaseIndex(){
 		switch(this.type){
-		
+
+		case CUSTOM: 
+			incPhaseIndexCustom(); 
+			break;
+
+		case VIOLIN:
+			this.phase_index += this.phase_increment;
+			break;	
+		case FLUTE:
+			this.phase_index += this.phase_increment;
+			break;
 		default:
 			this.phase_index += this.phase_increment; 
 		
